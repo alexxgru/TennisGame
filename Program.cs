cc27e6f40ef9d4279cb15e6d6c139448a66d2f38
alexxgru <alexgrundell@hotmail.com>
2023-04-29 11:50:06 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using TennisGame.ScoreBoard;
+
+Console.WriteLine("Hello, World!");
+
+bool running = true;
+ScoreBoard scoreBoard = new ScoreBoard(); 
+
+
+while (running)
+{
+    Console.WriteLine(scoreBoard.Score());
+
+    scoreBoard.AddScore();
+
+
+}
---
+++ ScoreBoard/ScoreBoard.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TennisGame.ScoreBoard
+{
+    public class ScoreBoard
+    {
+        public int ScorePlayer1 { get; set; } = 0;
+        public int ScorePlayer2 { get; set; } = 0;
+
+        public ScoreBoard() { }
+
+        public string Score()
+        {
+            return ScorePlayer1 + " " + ScorePlayer2; 
+        }
+
+        public void AddScore()
+        {
+            int selectedPlayer = ShowMenu.Menu("Which Player scored?", new[] {"Player 1", "Player 2"});
+
+            switch (selectedPlayer)
+            {
+                case 0:
+                    {
+                        ScorePlayer1++;
+                        break;
+                    }
+                case 1: 
+                    { 
+                    ScorePlayer2++; break;
+                    }
+            }
+        }
+    }
+}
